16231b9bdeb729d4cbdf8aa85940277435efe14b
Khizar Hasan <khizarhasan925@gmail.com>
2026-05-29 15:09:41 +0530

    update

--- page-templates/flexible.php
+++ page-templates/flexible.php
@@ -13,7 +13,7 @@ $sections = get_field('sections');
 <div id="content">
   <?php if(!empty($sections)): ?>
         <?php foreach ($sections as $index => $section): ?>
-            <?php $id = 'section-'.$index + 1; ?>
+            <?php $id = 'section-' . ($index + 1); ?>
             <?php get_template_part('template-parts/sections/' . $section['acf_fc_layout'], $section['type'] ?? 'default', array_merge($section, ['id' => $id])); ?>
         <?php endforeach; ?>
     <?php endif; ?>
