66411d3841980bc51dc5a7b2c2bcd574c25db4fd
Khizar Hasan <khizarhasan925@gmail.com>
2026-06-09 16:51:43 +0530

    update

--- template-parts/sections/form.php
+++ template-parts/sections/form.php
@@ -9,7 +9,7 @@ $template_part_name = explode('.', basename(__FILE__))[0];
 
 ?>
 
-<section id="<?php echo !empty($args['id']) ? $args['id'] : null; ?>" class="my-16 md:my-24 bg-white" data-section-id="<?php echo esc_attr($template_part_name); ?>">
+<section id="<?php echo !empty($args['id']) ? $args['id'] : null; ?>" class="container my-16 md:my-24 bg-white" data-section-id="<?php echo esc_attr($template_part_name); ?>">
     <div class="max-w-5xl mx-auto pt-10 pb-4 px-8 border border-zinc-300 bg-zinc-100">
         <?php if (!empty($args['title'])) : ?>
             <h2 class="text-2xl sm:text-[1.625rem] font-bold text-zinc-800 mb-8 text-center"><?php echo esc_html($args['title'] ?? ''); ?></h2>
